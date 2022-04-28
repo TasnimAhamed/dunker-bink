951e1f3b78b42f34a05492a1939466884aed2236
TasnimAhmed <tasnim.ahamed.20@gmail.com>
2022-04-28 19:01:33 +0600

    scrollspy add

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ window.onscroll=()=>{
     }
     
 }   
+
+//Scroll Spy
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#main'
+  })
+
+  var dataSpyList = [].slice.call(document.querySelectorAll('[data-bs-spy="scroll"]'))
+dataSpyList.forEach(function (dataSpyEl) {
+  bootstrap.ScrollSpy.getInstance(dataSpyEl)
+    .refresh()
+})
